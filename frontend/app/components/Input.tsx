0ef40e131ad156b57b1817364459da1034db587c
Eduardo Pozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-02-22 19:35:26 -0300

    Adicionando twmerge para permitir a estilização individual de cada input caso necessário

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 type InputProps = {
     label?: string;
@@ -7,6 +9,7 @@ type InputProps = {
     onChange?: (e:React.ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
     name?: string;
+    ClassName?: string;
 };
 
 export default function Input ({
@@ -16,9 +19,20 @@ export default function Input ({
     onChange,
     placeholder,
     name,
+    ClassName,
+
 }: InputProps) {
     return (
-        <div>
+        <div className="flex flex-1">
+            {label && <label>{label}</label>}
+            <input 
+                type={type} 
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                name={name}
+                className={twMerge("flex-1 border-gray-300 border-solid border-2 rounded-lg py-2 px-4", ClassName)}
+            />
         </div>
     )
 }
