6812384a8da49cbd53524bd06feea9f302682924
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:01 -0300

    Change the return values and implement new component formCard

--- frontend/app/components/UI/Login/LoginHeader.tsx
+++ frontend/app/components/UI/Login/LoginHeader.tsx
@@ -1,14 +1,14 @@
-import { ButtonLink } from "../General/Link"
+import { ButtonLink } from "../General/Link";
 
-export default function LoginHeader () {
+export default function LoginHeader() {
   return (
-  <div className="flex justify-evenly gap-5 mt-5 self-center w-full">
-        <ButtonLink content="Login" className="w-auto text-center"></ButtonLink>
+    <div className="flex justify-evenly gap-5 mt-5 self-center w-full">
+      <ButtonLink content="Login" className="w-auto text-center"></ButtonLink>
 
-        <ButtonLink
-          content="Cadastre-se"
-          className="bg-white text-gray-500 hidden md:inline"
-        ></ButtonLink>
-      </div>
-      );
+      <ButtonLink
+        content="Cadastre-se"
+        className="bg-white text-gray-500 hidden md:inline"
+      ></ButtonLink>
+    </div>
+  );
 }
