7f85a96a6aaea782978f14c8fc087ce9889b2c1e
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:00 -0300

    Refactoring the login page in small components for maintence and organization

---
+++ frontend/app/components/UI/Login/LoginBody.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "./LoginForm";
+import EntryButton from "./LoginEntryButton";
+import LoginHeader from "./LoginHeader";
+
+export default function LoginBody () {
+  return (
+    <section className="flex flex-col rounded-2xl bg-white shadow-lg gap-10 lg:gap-20 lg:py-8 lg:px-20 px-10 py-3 items-center">
+      <div className="flex flex-col gap-10 flex-nowrap">
+        <LoginHeader></LoginHeader>
+        <LoginForm></LoginForm>
+      </div>
+      <EntryButton></EntryButton>
+    </section>
+  );
+}
---
+++ frontend/app/components/UI/Login/LoginHeader.tsx
@@ -0,0 +1,14 @@
+import { ButtonLink } from "../General/Link"
+
+export default function LoginHeader () {
+  return (
+  <div className="flex justify-between gap-5 mt-5 self-center">
+        <ButtonLink content="Login" className="w-auto text-center"></ButtonLink>
+
+        <ButtonLink
+          content="Cadastre-se"
+          className="bg-white text-gray-500 hidden md:inline"
+        ></ButtonLink>
+      </div>
+      );
+}
