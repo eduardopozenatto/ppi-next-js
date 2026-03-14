e2a746e57fdaa458e8680c261d5970439a647c94
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:00 -0300

    Modify the input width responsiveness

--- frontend/app/components/UI/General/Input.tsx
+++ frontend/app/components/UI/General/Input.tsx
@@ -41,7 +41,7 @@ export default function Input ({
                 placeholder={placeholder}
                 name={name}
                 // utilizando o twMerge para juntar as classes definidas com as classes individuais (as individuais tem mais prioridade)
-                className={twMerge("flex-1 border-solid border-2 rounded-lg border-gray-300 hover:border-gray-400 focus:outline-none focus-visible:border-blue-400  py-2 px-4 transition-all duration-300 ease flex-nowrap", ClassName)}
+                className={twMerge("flex-1 border-solid border-2 rounded-lg border-gray-300 hover:border-gray-400 focus:outline-none focus-visible:border-blue-400  py-2 px-4 transition-all duration-300 ease w-full", ClassName)}
             />
         </div>
     )
