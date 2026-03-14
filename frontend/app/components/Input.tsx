8abe36118a1182e30a1905eff8229483eb0525e1
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:00 -0300

    comentando a funcionalidade do twmerge e do componente no geral, para melhor legibilidade

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import clsx from "clsx";
+// utilização do tailwind-merge para deixar o componente flexível permitindo sobrescrever suas classes conforme necessário
 import { twMerge } from "tailwind-merge";
+
+// tipo da função Input, com todos os valores necessários para o input
+
+// o ? na frente de cada variável significa que ela é opcional, e não obrigatória, 
+// o que possibilita omitir certas propriedades desnecessárias
 
 type InputProps = {
     label?: string;
@@ -12,6 +17,8 @@ type InputProps = {
     ClassName?: string;
 };
 
+// Função com a entrada dos valores do input
+
 export default function Input ({
     label,
     type = "text",
@@ -21,16 +28,19 @@ export default function Input ({
     name,
     ClassName,
 
-}: InputProps) {
+}: /* definindo o tipo do input com o tipo criado acima */ InputProps) {
     return (
         <div className="flex flex-1">
+            {/* label opcional para inserir junto ao input */}
             {label && <label>{label}</label>}
             <input 
+            // input com suas propriedades
                 type={type} 
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
                 name={name}
+                // utilizando o twMerge para juntar as classes definidas com as classes individuais (as individuais tem mais prioridade)
                 className={twMerge("flex-1 border-gray-300 border-solid border-2 rounded-lg py-2 px-4", ClassName)}
             />
         </div>
