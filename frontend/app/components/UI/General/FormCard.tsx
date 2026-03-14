56a2839a43cef5971e61ef6222acdd4974863abd
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:01 -0300

    delete the comment on the code

--- frontend/app/components/UI/General/FormCard.tsx
+++ frontend/app/components/UI/General/FormCard.tsx
@@ -7,9 +7,6 @@ type cardProps = {
 export default function FormCard ({content}: cardProps) {
   return (
     <section className="flex flex-col rounded-2xl bg-white shadow-lg gap-10 lg:gap-20 lg:py-8 lg:px-20 px-10 py-3 items-center w-full max-w-[600px] h-full">
-      {/* {content.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))} */}
       {content}
     </section>
   );
