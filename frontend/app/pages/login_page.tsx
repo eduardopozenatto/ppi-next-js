f7b6fc01a9bc90dfbe5a7b7148296ef99a2ae77f
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-14 18:50:00 -0300

    improve the layout of the login page and add more elements

--- frontend/app/pages/login_page.tsx
+++ frontend/app/pages/login_page.tsx
@@ -5,26 +5,33 @@ import { ButtonLink } from '../components/Link';
 const Aside = function () {
     return (
         <section className="flex justify-center">
-            <h1 className='text-6xl'>LabControl</h1>
+            <h1 className='text-6xl bg-[linear-gradient()] font-semibold'>LabControl</h1>
         </section>
     )
 }
 
 const LoginForm = function () {
     return (
-        <section className='flex flex-col border-solid border-2 rounded-2xl bg-white border-gray-700 gap-10 w-fit h-auto justify-center items-center mt-[10px] p-4'>
- 
-            <div className='flex justify-center gap-5 mt-2.5 '>
+        <section className='flex flex-col border-solid border-2 rounded-2xl bg-white border-gray-700 gap-10 max-w-[750px] px-10 py-3 items-center'>
+            
+            <div className='flex justify-between gap-5 mt-5'>
                 <ButtonLink content='Login'></ButtonLink>
-                <ButtonLink content='Cadastre-se' className=''></ButtonLink>
+                <ButtonLink content='Cadastre-se' className='bg-white text-gray-500'></ButtonLink>
             </div>
 
-            <div className='flex flex-col justify-center items-center w-[500px]'>
-                <form action = "" className = "flex flex-col gap-5 justify-center items-center" >
+            <div className='flex flex-col'>
+                <form action = "" className = "flex flex-col gap-5" >
                     <Input type='email' placeholder='Digite seu email: '></Input>
                     <Input type='password' placeholder='Insira sua senha: '></Input>
                 </form> 
-                <Link content='Esqueceu sua senha?' href='#' className='mt-2 mr-22 self-end'></Link>
+                <Link content='Esqueceu sua senha?' href='#' className='mt-2 self-end'></Link>
+            </div>
+
+            <div className='flex flex-col items-center gap-3 mt-25 mb-3'>
+                <ButtonLink content='Entrar'></ButtonLink>
+                <div>
+                    <p>Não possui cadastro? <Link content='clique aqui'></Link></p>
+                </div>
             </div>
 
         </section>
@@ -35,9 +42,9 @@ const LoginForm = function () {
 export function LoginPage() {
     return (
         <div className='flex flex-col bg-linear-to-br from-white via-blue-100 to-blue-200 h-screen justify-center items-center gap-8'>
+                
                 <Aside></Aside>
                 <LoginForm></LoginForm>
-
                 
         </div>
     );
