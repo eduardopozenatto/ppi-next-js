a227867b30ea7c80aaf52c20500176617b12f603
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-15 14:54:41 -0300

    Feature: change the form link based which page are the user in using useState passed by his father component

--- frontend/app/components/UI/General/EntryButton.tsx
+++ frontend/app/components/UI/General/EntryButton.tsx
@@ -1,13 +1,28 @@
+import { Dispatch, SetStateAction } from "react";
 import { SmButtonLink } from "./Link";
 import { Link } from "./Link";
 
-export default function EntryButton () {
+interface EntryButtonProps {
+  isLogin: boolean
+}
+
+export default function EntryButton ({isLogin}: EntryButtonProps){
   return (
     <div className="flex flex-col items-center gap-3 mt-10 mb-3">
       <SmButtonLink content="Entrar" />
       <div>
         <p>
-          Não possui cadastro? <Link content="clique aqui" />
+          {
+           isLogin ? (
+            <>
+              Não possui cadastro? {<Link content="Clique aqui"/>}
+            </>
+          ) : (
+            <>
+           Já possui cadastro? {<Link content="Clique aqui" />}
+            </>
+          )}
+
         </p>
       </div>
     </div>
