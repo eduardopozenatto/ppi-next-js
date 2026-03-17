a118384c60121381340ef37d5fb98d14cd073435
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-17 16:58:27 -0300

    feature: Added a page route in dashboard folder to start develop labcontrol system interfaces

---
+++ frontend/app/Dashboard/page.tsx
@@ -0,0 +1,17 @@
+import Sections from "@/components/Body/Sections";
+import Slogan from "../../components/Body/Slogan";
+import Storage from "./storage/storage";
+
+export default function Home() {
+  return (
+    <section className="h-screen grid grid-cols-[minmax(250px,1fr)_4fr]">
+      <div className="flex flex-col gap-2 bg-blue-50 rounded-r-4xl">
+        <Slogan />
+        <hr className="w-[80%] self-center text-gray-400"></hr>
+        <Sections />
+        
+      </div>
+      <Storage />
+    </section>
+  )
+}
