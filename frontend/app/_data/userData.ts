b02b2f74aa631dea0db6aceb02f401982b0431ec
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-17 16:57:05 -0300

    feature: Created a userdata file to control userdata values

---
+++ frontend/app/_data/userData.ts
@@ -0,0 +1,18 @@
+export const userData = {
+  id: 0,
+  tag: 'laboratorista',
+  userPermissions: {
+    verItens: true,
+    pedirEmprestimos: true,
+    VerNotificacoes: true,
+    VerEstoque: true,
+    GerarRelatorios: true,
+    AprovEmprestimos: true,
+    CriarItens: true,
+    GerenciarItens: true,
+    GerenciarUsuarios: true,
+    GerenciarTag: true,
+    GerenciarCat: true,
+    GerenciarPermissoes: true,
+  }
+}
