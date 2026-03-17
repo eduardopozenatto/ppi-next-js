f14fe18f565425ece391e1f2ce7ac1439642bfd8
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-16 23:32:09 -0300

    feature: added an function named validation to validate what mode the user is in to alter buttonLink properties

--- frontend/app/EntryPage/_components/Header.tsx
+++ frontend/app/EntryPage/_components/Header.tsx
@@ -1,18 +1,26 @@
 "use client"
 
 import { ButtonLink } from "../../../components/Button/Button";
-import { setModeProps } from "../../../components/Props/props";
+import { modeProps, setModeProps } from "../../../components/Props/props";
 
 
 export function Header({mode, setMode}: setModeProps) {
   const classes = "bg-white text-gray-500 md:inline";
+  function validation () {
+    if (mode == 'recovery') {
+      return '';
+
+    } else {
+      return setMode('login');
+    }
+  }
 
   return (
-    <div className="flex justify-evenly gap-5 mt-5 w-full items-center">
+    <div className= {mode == 'recovery' ? "flex w-full text-center justify-center mt-5 items-center" : "flex justify-evenly gap-5 mt-5 w-full items-center"}>
       <ButtonLink 
-        content="Login" 
-        onClick={() => {setMode('login')}} 
-        className={mode == 'recovery' ? "max-w-50" : mode == 'register' ? classes : "max-w-40 text-center"} 
+        content={mode == 'recovery' ? "Recuperação de senha" : "Login"} 
+        onClick={validation} 
+        className={mode == 'recovery' ? "w-full" : mode == 'register' ? classes : "max-w-40 text-center"} 
       />
 
       <ButtonLink
