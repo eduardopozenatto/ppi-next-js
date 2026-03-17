3a22df93f5d1deaf6b34ef3ea3e404e8dc8ad66a
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-16 22:53:56 -0300

    improve: Moved 'mode' useState to top-high component, to all components access it

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
+import { useState } from 'react';
+import { Mode } from '../components/Props/props';
+import Recovery from './Recovery/recovery';
 import EntryPage from './EntryPage/entrypage';
-// import Recovery from "./EntryPage/Recovery";
 
 export default function Home() {
+  const [mode, setMode] = useState<Mode>('login');
   return (
     <div>
-      <EntryPage />
+      { 
+        mode == 'recovery' ? 
+        <Recovery mode={mode} setMode={setMode}/> :
+        <EntryPage />
+      }
     </div>
   );
 }
