6974261ae6c2dd196aa9c289b79e6331aceeebce
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-19 20:20:54 -0300

    new/feature: write the items display algoritm

--- frontend/app/dashboard/storage/_components/items.tsx
+++ frontend/app/dashboard/storage/_components/items.tsx
@@ -1,3 +1,36 @@
+import { itemsData } from "@/app/_data/itemsData";
+import Image from "next/image";
+import { ButtonLink, SmButtonLink } from "@/components/Button/Button";
+
 export default function Items() {
-  
+  return (
+    <section className="grid lg:grid-cols-4 text-justify gap-10">
+    {Object.entries(itemsData).map(([key, item]) => (
+      <div key={key} className="flex items-center min-w-[400px] gap-10 p-4 border-2 border-gray-300 rounded-lg">
+        
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-end"> 
+          <SmButtonLink content="Disponível"></SmButtonLink>
+          
+          
+        </div>
+          <div>
+
+            <p key={key} className="text-md font-semibold text-gray-500">{item.name}</p>
+            <p className="text-sm text-gray-400 font-semibold">{item.category}</p>
+          </div>
+
+          <div>
+            <p>{item.description}</p>
+          </div>
+
+        </div>
+
+        <div>
+          <Image src={item.image} width={300} height={300} alt="icon"/>
+        </div>
+      </div>
+    ))}
+    </section>
+  )
 }
