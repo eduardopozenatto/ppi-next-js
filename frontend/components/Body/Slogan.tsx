93a9670a5627d4afac7dc8a925ae73eb8ac16c44
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-19 20:22:05 -0300

    sm-fix: added bold font weight in labcontrol slogan

--- frontend/components/Body/Slogan.tsx
+++ frontend/components/Body/Slogan.tsx
@@ -1,7 +1,7 @@
 export default function Slogan() {
   return (
     <div className="mt-5">
-      <p className="text-2xl text-center">LabControl</p>
+      <p className="text-2xl text-center font-semibold">LabControl</p>
     </div>
   )
 }
