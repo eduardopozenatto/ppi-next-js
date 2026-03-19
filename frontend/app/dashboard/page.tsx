87d785db9176e3c48fcf43c11ad6fa74cf56247e
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-18 22:14:23 -0300

    improve: fix the layout grid of the sidebar and pages

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Storage from "./storage/storage";
 
 export default function Home() {
   return (
-    <section className="h-screen grid grid-cols-[minmax(250px,1fr)_4fr]">
+    <section className="h-screen grid grid-cols-[minmax(250px,1fr)_10fr]">
       <div className="flex flex-col gap-2 bg-blue-50 rounded-r-4xl">
         <Slogan />
         <hr className="w-[80%] self-center text-gray-400"></hr>
