c83c73555fe37401a27e3de254ddfe3f974716af
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-20 23:37:20 -0300

    fix: changed the imports in page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import { useState } from 'react';
-import { Mode } from '../components/Props/props';
+import { Mode } from './types';
 import Recovery from './Recovery/recovery';
-import EntryPage from './EntryPage/entrypage';
+import EntryPage from './EntryPage/page';
 
 export default function Home() {
   const [mode, setMode] = useState<Mode>('login');
   return (
     <div>
       { 
-        mode == 'recovery' ? 
-        <Recovery mode={mode} setMode={setMode}/> :
-        <EntryPage />
+        mode === 'recovery' ? 
+        <Recovery /> :
+        <EntryPage mode={mode} setMode={setMode}/>
       }
     </div>
   );
