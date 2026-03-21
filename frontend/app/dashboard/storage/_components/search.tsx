3403305c0b890fa2ee88eeb8a8bbcb975f1cafa5
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-20 23:40:48 -0300

    fix: renamed ClassName property to className to more concise

--- frontend/app/dashboard/storage/_components/search.tsx
+++ frontend/app/dashboard/storage/_components/search.tsx
@@ -16,16 +16,16 @@ export default function SearchTab() {
     <section className="flex flex-col h-auto">
       <div className="grid grid-cols-[minmax(300px,1fr)_200px] gap-8">
         <div className="">
-          <Input type="text" placeholder="Buscar por nome ou descrição..." ClassName="focus-visible:border-gray-400 "/>
+          <Input type="text" placeholder="Buscar por nome ou descrição..." className="focus-visible:border-gray-400"/>
         </div>
-          <div className="flex justify-evenly items-center gap-4 border-2 border-gray-300 rounded-lg hover:border-gray-400 1s ease transition-all">
+        <div className="flex justify-evenly items-center gap-4 border-2 border-gray-300 rounded-lg hover:border-gray-400 transition-all duration-300 ease-in-out cursor-pointer">
           <Image src='/filter.svg' alt="icon" width={14} height={14}/>
           <p>todos</p>
           {isActive ? <Image src='/angle-down.svg' alt="icon" width={12} height={12} onClick={handleActive}/> : <Image src='/angle-up.svg' alt="icon" width={12} height={12} onClick={handleActive}/>}
         </div>
       </div>
 
-      {isActive && <div className="absolute top-45 flex self-end w-50 h-auto border-2 justify-center border-gray-300 mt-3 rounded-md">
+      {isActive && <div className="absolute top-44 flex self-end w-48 h-auto border-2 justify-center bg-white border-gray-300 mt-3 rounded-md shadow-md z-10">
         <ShowCat />
       </div>}
     </section>
