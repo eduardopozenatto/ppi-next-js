265aac32d3c8994458508c401324d87837dd383e
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-20 23:42:11 -0300

    new: Added recovery form to recovery password page and call it in recovery.tsx

--- frontend/app/Recovery/recovery.tsx
+++ frontend/app/Recovery/recovery.tsx
@@ -1,18 +1,15 @@
-// import FormBody from '../EntryPage/_components/FormBody';
-import FormCard from '@/components/Body/FormCard';
 import Title from '../EntryPage/_components/Title';
-import { Header } from '../EntryPage/_components/Header';
-import { setModeProps } from '@/components/Props/props';
+import RecoveryForm from './_components/RecoveryForm';
 
-export default function Recovery({mode, setMode}: setModeProps) {
+export default function Recovery() {
   return (
       
   <section className="flex justify-center items-center bg-linear-to-br from-white via-blue-100 to-blue-200 h-screen">
     <div className="flex flex-col lg:flex-row lg:justify-evenly lg:max-w-400 w-full items-center gap-10 mx-10 h-fill">
-      <Title></Title>
-      <FormCard 
-        content={[<Header mode={mode} setMode={setMode} key={0}/>, ]}
-      />
+      <Title />
+      <div className="w-full max-w-md">
+        <RecoveryForm />
+      </div>
     </div>
   </section>
   );
